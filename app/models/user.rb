59b76d9c340c9c7a156cd5277a482838192176f1
Set27 <matvei.2002@mail.ru>
2025-01-12 21:28:52 +0300

    Use helpers for validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :patronymic, :email, :age, :nationality, :gender
   validates_uniqueness_of :email
-  validates :age, inclusion: { in: 1..90, message: "must be between 1 and 90" }
-  validates :gender, inclusion: { in: %w[male female], message: "must be either 'male' or 'female'" }
+  validates_inclusion_of :age, in: 1..90, message: "must be between 1 and 90"
+  validates_inclusion_of :gender, in: %w[male female], message: "must be either 'male' or 'female'"
 
   def full_name
     "#{name} #{patronymic}"
