1e9f116d6d6eabfbf5b418ef568e8d5c3ee9d577
Set27 <matvei.2002@mail.ru>
2025-01-12 22:24:53 +0300

    Add shoulda matcher, rewrite model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,94 +4,27 @@ RSpec.describe User, type: :model do
   let(:user) { build(:user) }
 
   describe 'validations' do
-    it 'is valid with valid attributes' do
-      expect(user).to be_valid
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:patronymic) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:age) }
+    it { should validate_presence_of(:nationality) }
+    it { should validate_presence_of(:gender) }
 
-    it 'is not valid without a name' do
-      user.name = nil
-      expect(user).not_to be_valid
-      expect(user.errors[:name]).to include("can't be blank")
-    end
+    it { should validate_uniqueness_of(:email).case_insensitive }
 
-    it 'is not valid without a patronymic' do
-      user.patronymic = nil
-      expect(user).not_to be_valid
-      expect(user.errors[:patronymic]).to include("can't be blank")
-    end
+    it { should validate_inclusion_of(:age).in_range(1..90).with_message("must be between 1 and 90") }
 
-    it 'is not valid without an email' do
-      user.email = nil
-      expect(user).not_to be_valid
-      expect(user.errors[:email]).to include("can't be blank")
-    end
-
-    it 'is not valid without an age' do
-      user.age = nil
-      expect(user).not_to be_valid
-      expect(user.errors[:age]).to include("can't be blank")
-    end
-
-    it 'is not valid without a nationality' do
-      user.nationality = nil
-      expect(user).not_to be_valid
-      expect(user.errors[:nationality]).to include("can't be blank")
-    end
-
-    it 'is not valid without a gender' do
-      user.gender = nil
-      expect(user).not_to be_valid
-      expect(user.errors[:gender]).to include("can't be blank")
-    end
-
-    it 'is not valid with a duplicate email' do
-      create(:user, email: 'test@example.com')
-      user.email = 'test@example.com'
-      expect(user).not_to be_valid
-      expect(user.errors[:email]).to include('has already been taken')
-    end
-
-    it 'is not valid with an age less than 1' do
-      user.age = 0
-      expect(user).not_to be_valid
-      expect(user.errors[:age]).to include('must be between 1 and 90')
-    end
-
-    it 'is not valid with an age greater than 90' do
-      user.age = 91
-      expect(user).not_to be_valid
-      expect(user.errors[:age]).to include('must be between 1 and 90')
-    end
-
-    it 'is not valid with an invalid gender' do
-      user.gender = 'other'
-      expect(user).not_to be_valid
-      expect(user.errors[:gender]).to include('must be either \'male\' or \'female\'')
-    end
+    it { should allow_values('male', 'female').for(:gender) }
+    it { should_not allow_values('other', 'unknown').for(:gender) }
   end
 
   describe 'associations' do
-    it 'has many user_interests' do
-      association = described_class.reflect_on_association(:user_interests)
-      expect(association.macro).to eq :has_many
-    end
+    it { should have_many(:user_interests).dependent(:destroy) }
+    it { should have_many(:interests).through(:user_interests) }
 
-    it 'has many interests through user_interests' do
-      association = described_class.reflect_on_association(:interests)
-      expect(association.macro).to eq :has_many
-      expect(association.options[:through]).to eq :user_interests
-    end
-
-    it 'has many user_skills' do
-      association = described_class.reflect_on_association(:user_skills)
-      expect(association.macro).to eq :has_many
-    end
-
-    it 'has many skills through user_skills' do
-      association = described_class.reflect_on_association(:skills)
-      expect(association.macro).to eq :has_many
-      expect(association.options[:through]).to eq :user_skills
-    end
+    it { should have_many(:user_skills).dependent(:destroy) }
+    it { should have_many(:skills).through(:user_skills) }
   end
 
   describe 'custom methods' do
