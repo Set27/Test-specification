e1260d14d2e38ec9940c20cb5ed246aa2049217b
Set27 <matvei.2002@mail.ru>
2025-01-13 12:01:42 +0300

    Init user controller and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,70 +1,21 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
-
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users or /users.json
   def create
-    @user = User.new(user_params)
+    outcome = Users::Create.run(user_params)
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+      if outcome.valid?
+        result = outcome.result
+        format.json { render json: result, status: :created }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        @errors = result.errors
+        format.json { render json: @errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to users_path, status: :see_other, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.expect(user: [ :name, :patronymic, :email, :age, :nationality, :country, :gender ])
-    end
+  def user_params
+    params.require(:user).permit(:name, :patronymic, :email, :age, :nationality, :country, :gender, interests: [], skills: "")
+  end
 end
---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "POST /users" do
+    subject(:do_request) { post "/users", params: { user: params }, as: :json }
+
+    let(:params) { valid_params }
+    let(:valid_params) do
+      {
+        name: "John",
+        patronymic: "Doe",
+        email: "john@example.com",
+        age: 30,
+        nationality: "American",
+        country: "USA",
+        gender: "male",
+        interests: ["Programming", "Reading"],
+        skills: "Ruby, Rails"
+      }
+    end
+
+    context "when valid params" do
+      let(:created_user) { User.last }
+
+      it 'returns correct status code with response' do
+        do_request
+
+        expect(response).to have_http_status(:created)
+        expect(response.parsed_body).to include(
+          "id" => created_user.id,
+          "name" => created_user.name,
+          "patronymic" => created_user.patronymic,
+          "email" => created_user.email,
+          "age" => created_user.age,
+          "nationality" => created_user.nationality,
+          "country" => created_user.country,
+          "gender" => created_user.gender
+        )
+      end
+
+      it 'creates new user in DB' do
+        expect { do_request }.to change { User.count }.by(1)
+      end
+    end
+
+    context "when validation error" do
+      let(:params) { valid_params.merge(name: "", email: "") }
+
+      it 'returns unprocessable entity status code with errors messages' do
+        do_request
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.parsed_body).to be_present
+      end
+
+      it 'does not create new user' do
+        expect { do_request }.not_to change { User.count }
+      end
+    end
+  end
+end
